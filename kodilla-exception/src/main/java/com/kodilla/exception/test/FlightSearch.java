10d11f32e37ffde0762dfdf3f3de2a6c48554dfb
Mariusz Pawłowski <mpawlowski15@interia.pl>
2022-08-14 11:25:49 +0200

    8.4 Added kodilla-Error handling module

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -0,0 +1,36 @@
+package com.kodilla.exception.test;
+import java.util.HashMap;
+import java.util.Set;
+
+public class FlightSearch {
+    private static boolean checkIfExistsOnMap (Set<String> airportSet, String comparedAirport) {
+        boolean isOnMap = airportSet.stream()
+                .anyMatch(n -> n.equals(comparedAirport));
+        System.out.println(comparedAirport + " exists on map: " + isOnMap);
+        return isOnMap;
+    }
+
+    public final void findFlight(Flight flight) throws RouteNotFoundException {
+
+        final HashMap<String, Boolean> availability = new HashMap<>();
+        availability.put("London", true);
+        availability.put("Warsaw", true);
+        availability.put("Berlin", false);
+
+        if (checkIfExistsOnMap(availability.keySet(), flight.arrivalAirport) && checkIfExistsOnMap(availability.keySet(), flight.departureAirport)) {
+            System.out.println("System continues to work..");
+        } else {
+            throw new RouteNotFoundException("no such airport on map");
+        }
+    }
+    public static void main(String[] args) {
+
+        FlightSearch flightSearch = new FlightSearch();
+
+        try {
+            flightSearch.findFlight(new Flight("London", "Warsaw"));
+        } catch (RouteNotFoundException e) {
+            System.out.println("We don't recognize your destination airport");
+        }
+    }
+}
