4325d3bcd5eae5dcd3a0be754ec1026e63702d93
Mariusz Pawłowski <mpawlowski15@interia.pl>
2023-01-03 22:10:19 +0100

    24.3

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderWatcher.java
@@ -0,0 +1,44 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class OrderWatcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))"
+            + "&& args(order, userId) && target(object)")
+    public void logBefore(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", Args: " + order + " Order Id = " + userId);
+    }
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public OrderFacade measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        OrderFacade result;
+        try {
+            long begin = System.currentTimeMillis();
+            result = (OrderFacade) proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("Time consumed " + (end - begin) + "[ms]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+    }
+
+    @After(value = "execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))"
+            + "&& args(order, userId) && target(object)", argNames = "order,userId,object")
+    public void logAfter(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", Args: " + order + " Order Id = " + userId);
+    }
+}
